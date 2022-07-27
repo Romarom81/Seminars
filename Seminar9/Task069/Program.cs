bc25972b36611c93ef0183cc24f48afa90fce459
Romarom81 <romarom81@mail.ru>
2022-07-27 18:50:04 +0300

    add Siminar9

--- Seminar9/Task069/Program.cs
+++ Seminar9/Task069/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа,
+// и возводит число А в целую степень В с помощью рекурсии. 
+
+int A = 5;
+int B = 5;
+Recursion(A, B);
+
+void Recursion(int A, int B, int result = 1)
+{
+  if (B == 0)
+  {
+    Console.WriteLine(result);
+    return;
+  }
+  result *= A;
+  B--;
+  Recursion(A, B, result);
+}
